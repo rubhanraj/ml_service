f34546d76a6099f426dc2a62a7c40f8120966870
rubhan xavier raj <rubhanxavierraj@rubhans-MBP.default>
2022-06-15 23:20:48 +0100

    Changes to dockerfile and requirements.txt to make the build and compose up work

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '753qbb44sp6kf08c6a-ust_l3*qo-@hkca0sk1tw7xgnzsz*zv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
